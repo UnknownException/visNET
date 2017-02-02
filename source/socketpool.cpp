973df94a11a926bf42897ebb8d185f8bc0fb13b2
UnknownException <UnknownException@users.noreply.github.com>
2017-02-02 11:09:20 +0100

    Implement socketpool sendPacket Set socketpool socket to nonblocking Changed addSocket to return an identifier

--- source/socketpool.cpp
+++ source/socketpool.cpp
@@ -20,46 +20,62 @@ namespace visNET{
 
 	auto SocketPool::getFreeID() -> decltype(m_nSocketID)
 	{ 
-		//Should only be accessed from the ::run thread
 		decltype(m_nSocketID) id = 0;
 
+		m_mutReserveID.lock();
 		if (m_queReserveID.empty())
-			id = m_nSocketID++;
+			id = ++m_nSocketID;
 		else
 		{
 			id = m_queReserveID.front();
 			m_queReserveID.pop();
 		}
+		m_mutReserveID.unlock();
 
 		return id;
 	}
 
-	bool SocketPool::addSocket(std::unique_ptr<Socket> s)
+	auto SocketPool::addSocket(std::unique_ptr<Socket> s) -> decltype(m_nSocketID)
 	{
 		if (s->getSocket() == INVALID_SOCKET)
-			return false;
+			return 0;
+
+		s->setNonBlocking(true);
+		auto id = getFreeID();
 
 		m_mutNewSockets.lock();
-			m_vecNewSockets.push_back(std::move(s));
+			m_vecNewSockets.push_back(std::make_pair(id, std::move(s)));
 		m_mutNewSockets.unlock();
 
-		return true;
+		return id;
 	}
 
-	auto SocketPool::getPackets() -> decltype(m_vecPackets)
+	void SocketPool::removeSocket(decltype(m_nSocketID) nSocketID)
 	{
-		decltype(m_vecPackets) packets;
+		//Disconnect
+	}
+
+	auto SocketPool::getPackets() -> decltype(m_vecRecvPackets)
+	{
+		decltype(m_vecRecvPackets) packets;
 
 		if (!m_bPacketsAvailable)
 			return packets;
 
-		m_mutPackets.lock();
-			packets.swap(m_vecPackets);
-		m_mutPackets.unlock();
+		m_mutRecvPackets.lock();
+			packets.swap(m_vecRecvPackets);
+		m_mutRecvPackets.unlock();
 
 		m_bPacketsAvailable = false;
 
 		return packets;
+	}
+
+	void SocketPool::sendPacket(decltype(m_nSocketID) nSocketID, std::shared_ptr<RawPacket> pPacket)
+	{
+		m_mutSendPackets.lock();
+			m_vecSendPackets.push_back(std::make_pair(nSocketID, pPacket));
+		m_mutSendPackets.unlock();
 	}
 
 	void SocketPool::run()
@@ -68,31 +84,53 @@ namespace visNET{
 		{
 			m_mutNewSockets.lock();
 				for (auto it = m_vecNewSockets.begin(); it != m_vecNewSockets.end(); ++it)
-					m_vecSockets.push_back(std::make_pair(getFreeID(), std::move(*it)));
+					m_vecSockets.push_back(std::make_pair((*it).first, std::move((*it).second)));
 
 				m_vecNewSockets.clear();
 			m_mutNewSockets.unlock();
 
-			decltype(m_vecPackets) packets;
+			decltype(m_vecRecvPackets) packets;
 			for (auto it = m_vecSockets.begin(); it != m_vecSockets.end();)
 			{
 				auto packet = std::make_shared<RawPacket>();
 				if ((*it).second->read(*packet))
 					packets.push_back(std::make_pair((*it).first, packet));
-				
+
+				m_mutSendPackets.lock();
+					if (!m_vecSendPackets.empty())
+					{
+						for (auto snd_it = m_vecSendPackets.begin(); snd_it != m_vecSendPackets.end();)
+						{
+							if ((*it).first == (*snd_it).first)
+							{
+								(*it).second->write(*(*snd_it).second);
+								snd_it = m_vecSendPackets.erase(snd_it);
+							}
+							else
+								++snd_it;
+						}
+					}
+				m_mutSendPackets.unlock();
+
 				if (!(*it).second->getAlive())
 				{
-					m_queReserveID.push((*it).first);
+					m_mutReserveID.lock();
+						m_queReserveID.push((*it).first);
+					m_mutReserveID.unlock();
 					it = m_vecSockets.erase(it);
 				}
 				else
 					++it;
 			}
 		
-			m_mutPackets.lock();
-				m_vecPackets.insert(m_vecPackets.end(), packets.begin(), packets.end());
-				m_bPacketsAvailable = !m_vecPackets.empty();
-			m_mutPackets.unlock();
+			m_mutRecvPackets.lock();
+				if (!packets.empty())
+					m_vecRecvPackets.insert(m_vecRecvPackets.end(), packets.begin(), packets.end());
+
+				m_bPacketsAvailable = !m_vecRecvPackets.empty();
+			m_mutRecvPackets.unlock();
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(50)); //Replace with poll
 		}
 	}
 }
--- source/socketpool.h
+++ source/socketpool.h
@@ -3,20 +3,23 @@
 #include <atomic>
 #include <mutex>
 #include <thread>
-#include <algorithm>
 #include <queue>
 
 namespace visNET{
 	class SocketPool{
 		uint32_t m_nSocketID;
+		std::mutex m_mutReserveID;
 		std::queue<decltype(m_nSocketID)> m_queReserveID;
 
+		std::mutex m_mutSendPackets;
+		std::vector<std::pair<decltype(m_nSocketID), std::shared_ptr<RawPacket>>> m_vecSendPackets;
+
 		std::atomic<bool> m_bPacketsAvailable;
-		std::mutex m_mutPackets;
-		std::vector<std::pair<decltype(m_nSocketID), std::shared_ptr<RawPacket>>> m_vecPackets;
+		std::mutex m_mutRecvPackets;
+		std::vector<std::pair<decltype(m_nSocketID), std::shared_ptr<RawPacket>>> m_vecRecvPackets;
 
 		std::mutex m_mutNewSockets;
-		std::vector<std::unique_ptr<Socket>> m_vecNewSockets;
+		std::vector<std::pair<decltype(m_nSocketID), std::unique_ptr<Socket>>> m_vecNewSockets;
 		std::vector<std::pair<decltype(m_nSocketID), std::unique_ptr<Socket>>> m_vecSockets;
 
 		std::atomic<bool> m_bRun;
@@ -28,9 +31,11 @@ namespace visNET{
 	private:
 		auto getFreeID() -> decltype(m_nSocketID);
 	public:
-		bool addSocket(std::unique_ptr<Socket> s);
-		auto getPackets() -> decltype(m_vecPackets);
+		auto addSocket(std::unique_ptr<Socket> s) -> decltype(m_nSocketID);
+		void removeSocket(decltype(m_nSocketID));
 
+		auto getPackets() -> decltype(m_vecRecvPackets);
+		void sendPacket(decltype(m_nSocketID) nSocketID, std::shared_ptr<RawPacket> pPacket);
 	private:
 		void run();
 	};
