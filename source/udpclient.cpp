a06676ac470f325b0291a905af033ac8846993a5
UnknownException <UnknownException@users.noreply.github.com>
2018-06-08 13:56:50 +0200

    Add checksum to UDP messages

--- source/udpclient.cpp
+++ source/udpclient.cpp
@@ -4,7 +4,8 @@
 namespace visNETCore{
 	UdpClient::UdpClient(uint16_t nPort)
 	{
-		m_pBuffer = new uint8_t[_visNET_PACKETSIZE_LIMIT];
+		// Reserve buffer for one full packet and checksum
+		m_pBuffer = new uint8_t[_visNET_PACKETSIZE_LIMIT + sizeof(uint32_t)];
 
 		addrinfo hints, *result;
 		ZeroMemory(&hints, sizeof(addrinfo));
@@ -41,17 +42,21 @@ namespace visNETCore{
 
 	UdpClient::~UdpClient()
 	{
-		if (m_pBuffer)
-			delete[] m_pBuffer;
+		delete[] m_pBuffer;
 	}
 
 	bool UdpClient::send(UdpMessage& message) 
 	{
-		if (!isValid())
+		if (!isValid() || !message.getPacket())
 			return false;
 
 		message.getPacket()->_onSend(); 
-		return getSocket()->writeTo(message.getPacket()->_getRawData(), message.getPacket()->_getRawSize(), message.getIP().c_str(), message.getPort());
+
+		uint32_t checksum = generateChecksum(message.getPacket()->_getRawData(), message.getPacket()->_getRawSize());
+		memcpy(m_pBuffer, &checksum, sizeof(uint32_t));
+			
+		memcpy(m_pBuffer + sizeof(uint32_t), message.getPacket()->_getRawData(), message.getPacket()->_getRawSize());
+		return getSocket()->writeTo(m_pBuffer, message.getPacket()->_getRawSize() + sizeof(uint32_t), message.getIP().c_str(), message.getPort());
 	}
 
 	std::vector<UdpMessage> UdpClient::getPackets()
@@ -64,26 +69,50 @@ namespace visNETCore{
 		auto result = getSocket()->readFrom(m_pBuffer, _visNET_PACKETSIZE_LIMIT);
 		while (result.second > 0)
 		{
-			if (result.second >= sizeof(uint32_t))
+			// Check if packet atleast contains checksum and packetsize
+			if (result.second >= sizeof(uint32_t) + sizeof(uint32_t))
 			{
 				auto packet = std::make_shared<Packet>();
-				uint32_t nSize = 0;
-				memcpy(&nSize, m_pBuffer, sizeof(nSize));
-				if (nSize == result.second)
-				{
-					packet->_onReceive(m_pBuffer, result.second);
-					
-					char tmp[INET_ADDRSTRLEN];
-					InetNtop(AF_INET, &result.first.sin_addr, tmp, INET_ADDRSTRLEN);
+				uint32_t size = 0;
+				memcpy(&size, m_pBuffer + sizeof(uint32_t), sizeof(uint32_t));
 
-					retn.push_back(UdpMessage(std::string(tmp), result.first.sin_port, packet));
+				if (validatePacket(m_pBuffer, result.second, size))
+				{
+					if (packet->_onReceive(m_pBuffer + sizeof(uint32_t), size) == 0)
+					{
+						char tmp[INET_ADDRSTRLEN];
+						InetNtop(AF_INET, &result.first.sin_addr, tmp, INET_ADDRSTRLEN);
+
+						retn.push_back(UdpMessage(std::string(tmp), result.first.sin_port, packet));
+					}
 				}
-				// else the packet will be dropped
 			}
 
 			result = getSocket()->readFrom(m_pBuffer, _visNET_PACKETSIZE_LIMIT);
 		}
 
 		return retn;
+	}
+
+	bool UdpClient::validatePacket(uint8_t* buffer, uint32_t bufferSize, uint32_t expectedPacketSize)
+	{
+		// Check if received buffersize minus checksum is same as packetsize defined in packet
+		if (expectedPacketSize != bufferSize - sizeof(uint32_t))
+			return false;
+
+		uint32_t checksum = 0;
+		memcpy(&checksum, buffer, sizeof(uint32_t));
+
+		return (checksum == generateChecksum(buffer + sizeof(uint32_t), expectedPacketSize));
+	}
+
+	uint32_t UdpClient::generateChecksum(const uint8_t* buffer, const uint32_t bufferSize)
+	{
+		uint32_t checksum = 0;
+
+		for (uint32_t i = 0; i < bufferSize; ++i)
+			checksum += buffer[i] ^ (i % 256);
+
+		return checksum ^ 0x1234FEDC;
 	}
 }
--- source/udpclient.h
+++ source/udpclient.h
@@ -10,5 +10,8 @@ namespace visNETCore{
 
 		bool send(UdpMessage& message);
 		std::vector<UdpMessage> getPackets();
+	private:
+		bool validatePacket(uint8_t* buffer, uint32_t bufferSize, uint32_t expectedPacketSize);
+		uint32_t generateChecksum(const uint8_t* buffer, const uint32_t bufferSize);
 	};
 }
